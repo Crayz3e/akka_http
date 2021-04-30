a7818f42393f504c663c3b3ed4278cb48adedeb7
crayz3e <crayz3e@gmail.com>
2021-04-30 17:40:00 +0600

    fix

--- src/main/scala/Server.scala
+++ src/main/scala/Server.scala
@@ -6,11 +6,11 @@ import scala.concurrent.{ExecutionContext, Future}
 import scala.util.{Failure, Success, Try}
 
 object Server {
-  val host = "0.0.0.0"
+  val host = "localhost"
   val port = Try(System.getenv("PORT")).map(_.toInt).getOrElse(9000)
   def Start(routes: Route)(implicit system: ActorSystem[_], ex: ExecutionContext) {
-    val binding = Http().newServerAt(host, port).bind(routes: Route)
-    binding.onComplete {
+    val futurebinding = Http().newServerAt(host, port).bind(routes: Route)
+    futurebinding.onComplete {
       case Success(binding) =>
         val address = binding.localAddress
         system.log.info("Server online at http://{}:{}/", address.getHostString, address.getPort)
